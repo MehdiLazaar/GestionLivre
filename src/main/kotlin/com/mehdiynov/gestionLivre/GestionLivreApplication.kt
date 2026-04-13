76f7d24d94876bfd12f270a368974eb643ae6119
Mehdi <mehdilaz109@gmail.com>
2026-04-13 15:19:45 +0200

    Creation projet de : gestion de livre

---
+++ src/main/kotlin/com/mehdiynov/gestionLivre/GestionLivreApplication.kt
@@ -0,0 +1,11 @@
+package com.mehdiynov.gestionLivre
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestionLivreApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestionLivreApplication>(*args)
+}
